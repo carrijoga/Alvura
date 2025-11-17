f93626ddb77c0e1cf9bc7fc4de919260c5b261d1
Gabriel Carrijo <gabriel20carrijo@hotmail.com>
2025-11-17 14:06:14 -0300

    adjusting the navbar link for reports

--- Alvura/obj/Debug/net10.0/Alvura.AssemblyInfo.cs
+++ Alvura/obj/Debug/net10.0/Alvura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alvura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579f1f115b57803281f53f1288ba223b206e43e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8c13b5a2583cc7f35e1758708e396d9bf05cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alvura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alvura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
